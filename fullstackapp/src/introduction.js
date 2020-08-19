badbb7f4ae107d7052191a86732113b9156cbed9
ibrahim2020diallo <67746140+ibrahim2020diallo@users.noreply.github.com>
2020-08-19 12:18:03 -0400

    Covid-19
    Installation of react-bootstrap bootstrap
    Use of Navigation and router to cange pages.

--- fullstackapp/src/introduction.js
+++ fullstackapp/src/introduction.js
@@ -1,21 +1,24 @@
 import React from "react";
-import Introduction from "./introduction.js";
+import Navigation from "./Nav.js";
 
 export default function Introduction() {
   return (
     <div>
-      Coronavirus disease 2019 (COVID 19) is an infectious disease caused by
-      severe acute respiratory syndrome coronavirus 2 . It was first identified
-      in December 2019 in Wuhan, Hubei, China, and has resulted in an ongoing
-      pandemic. As of 14 August 2020, more than 21 million cases have been
-      reported across 188 countries and territories, resulting in more than
-      761,000 deaths. More than 13 million people have recovered. Common
-      symptoms include fever, cough, fatigue, shortness of breath, and loss of
-      smell and taste. While most people have mild symptoms, some people develop
-      acute respiratory distress syndrome possibly precipitated by cytokine
-      storm, multi-organ failure, septic shock, and blood clots. The time from
-      exposure to onset of symptoms is typically around five days, but may range
-      from two to fourteen days.
+      <h1>Introduction to Covid-19</h1>
+      <p>
+        Coronavirus disease 2019 (COVID 19) is an infectious disease caused by
+        severe acute respiratory syndrome coronavirus 2 . It was first
+        identified in December 2019 in Wuhan, Hubei, China, and has resulted in
+        an ongoing pandemic. As of 14 August 2020, more than 21 million cases
+        have been reported across 188 countries and territories, resulting in
+        more than 761,000 deaths. More than 13 million people have recovered.
+        Common symptoms include fever, cough, fatigue, shortness of breath, and
+        loss of smell and taste. While most people have mild symptoms, some
+        people develop acute respiratory distress syndrome possibly precipitated
+        by cytokine storm, multi-organ failure, septic shock, and blood clots.
+        The time from exposure to onset of symptoms is typically around five
+        days, but may range from two to fourteen days.
+      </p>
     </div>
   );
 }
